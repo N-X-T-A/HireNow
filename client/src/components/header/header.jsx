76febf25d6ea825d306d95cc51572e85fc226ae4
Thai <pumpo6969@gmail.com>
2025-01-13 15:11:15 +0700

    test updt

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -1,5 +1,27 @@
 import React from "react";
 import "../header/header.css";
-export default function Header() {
-  return <div className="">header</div>;
-}
+import "../../assets/home/logo.png";
+const Header = () => {
+  return (
+    <>
+      <header>
+        <div className="container-fluid flex justify-center">
+          <div className="row h-24 w-11/12">
+            {/* logo */}
+            <div className="col-lg-2 flex items-center justify-center">
+              <img
+                className="max-w-28 w-full "
+                src="/src/assets/home/logo.png"
+                alt=""
+              />
+            </div>
+            {/* right item */}
+            <div className=" col-lg-10 "></div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./index.css";
 import Home from "./pages/Home";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
