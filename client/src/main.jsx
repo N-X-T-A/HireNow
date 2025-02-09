3c48e5d321a25575769d3cfc1b41a79002c59085
Thai <pumpo6969@gmail.com>
2025-02-09 15:22:22 +0700

    Update UI/UX

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,18 +8,15 @@ import Test from "./pages/Test";
 import Login from "./pages/Login";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Environment } from "./environments/Environment";
-const CLIENT_ID = Environment.GG_CLIENT_ID;
 
 createRoot(document.getElementById("root")).render(
-  <GoogleOAuthProvider clientId={CLIENT_ID}>
-    <StrictMode>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/test" element={<Test />} />
-        </Routes>
-      </Router>
-    </StrictMode>
-  </GoogleOAuthProvider>
+  <StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/test" element={<Test />} />
+      </Routes>
+    </Router>
+  </StrictMode>
 );
--- client/src/pages/loginMethod.jsx
+++ client/src/pages/loginMethod.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Header from "../components/header/header";
-import { useGoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
+import {
+  useGoogleLogin,
+  GoogleOAuthProvider,
+  GoogleLogin,
+} from "@react-oauth/google";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { jwtDecode } from "jwt-decode";
 import { Environment } from "../environments/Environment";
@@ -22,103 +26,31 @@ export default function LoginMethod() {
   });
   const [isLoading, setIsLoading] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  //login google
-  const login = useGoogleLogin({
-    onSuccess: async (response) => {
-      setIsLoading(true);
 
-      const { access_token } = response;
-      try {
-        const userInfoResponse = await fetch(
-          "https://www.googleapis.com/oauth2/v3/userinfo",
-          {
-            headers: {
-              Authorization: `Bearer ${access_token}`,
-            },
-          }
-        );
-        const userInfo = await userInfoResponse.json();
+  // const handleGoogleLogin = useGoogleLogin({
+  //   onSuccess: async (response) => {
+  //     try {
+  //       const res = await axios.post(
+  //         "http://localhost:5000/auth/google",
+  //         {
+  //           token: response.credential,
+  //         },
+  //         { withCredentials: true }
+  //       );
 
-        sessionStorage.setItem("access_token", access_token);
-        sessionStorage.setItem("user", JSON.stringify(userInfo));
-        setUser(userInfo);
-        setTimeout(() => {
-          setIsLoading(false);
-          setIsLoggedIn(true);
-        }, 2000);
-      } catch (error) {
-        console.error("Error fetching user info:", error);
-        alert("Lấy thông tin người dùng thất bại");
-        setIsLoading(false); // Tắt loading nếu có lỗi
-      }
-    },
-    onError: (error) => {
-      console.error("Google login error:", error);
-      alert("Đăng nhập thất bại");
-      setIsLoading(false);
-    },
-  });
-  //login git
-  const handleLoginGit = () => {
-    const authUrl = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&scope=user&redirect_uri=${REDIRECT_URI}`;
-    window.location.href = authUrl;
-  };
-  useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const code = urlParams.get("code");
+  //       if (res.data.success) {
+  //         setTimeout(() => {
+  //           setIsLoading(false);
+  //           setIsLoggedIn(true);
+  //         }, 2000);
+  //       }
+  //     } catch (error) {
+  //       console.error("Login Failed", error);
+  //     }
+  //   },
+  //   onError: () => console.log("Login Failed"),
+  // });
 
-    if (code) {
-      console.log("GitHub Authorization Code:", code);
-      alert("Lấy code thành công! Code: " + code);
-      //Xóa path sau 1 lan success
-      window.history.replaceState({}, document.title, window.location.pathname);
-      // setIsLoading(true);
-      // fetch(`https://github.com/login/oauth/access_token`, {
-      //   method: "POST",
-      //   headers: {
-      //     "Content-Type": "application/json",
-      //     Accept: "application/json",
-      //   },
-      //   body: JSON.stringify({
-      //     client_id: GITHUB_CLIENT_ID,
-      //     client_secret: GITHUB_SECRET_ID,
-      //     code,
-      //     redirect_uri: REDIRECT_URI,
-      //   }),
-      // })
-      //   .then((res) => res.json())
-      //   .then(async (data) => {
-      //     console.log("GitHub response:", data);
-      //     if (data.access_token) {
-      //       sessionStorage.setItem("access_token", data.access_token);
-
-      //       // Lấy thông tin người dùng từ GitHub API
-      //       const userInfoResponse = await fetch(
-      //         "https://api.github.com/user",
-      //         {
-      //           headers: {
-      //             Authorization: `Bearer ${data.access_token}`,
-      //           },
-      //         }
-      //       );
-
-      //       const userInfo = await userInfoResponse.json();
-      //       console.log("User info:", userInfo);
-      //       sessionStorage.setItem("user", JSON.stringify(userInfo));
-      //       setUser(userInfo);
-      //       setIsLoggedIn(true);
-      //     } else {
-      //       console.error("GitHub OAuth failed:", data);
-      //       alert("Đăng nhập GitHub thất bại");
-      //     }
-      //   })
-      //   .catch((error) => {
-      //     console.error("Error during GitHub login:", error);
-      //     alert("Đăng nhập GitHub thất bại");
-      //   })
-      //   .finally(() => setIsLoading(false));
-    }
-  }, []);
   //Lấy token local
   const accessToken = sessionStorage.getItem("access_token");
   const userif = sessionStorage.getItem("user");
@@ -173,7 +105,7 @@ export default function LoginMethod() {
               </div>
               <div className="flex  gap-[10px] flex-col items-center w-[70%] py-[25px]">
                 <button
-                  onClick={login}
+                  // onClick={handleGoogleLogin}
                   className="flex items-center justify-items-center justify-center gap-[10px] w-full h-[45px] rounded-[10px] px-[20px] py-[10px] border-[1px] border-zinc-800 text-black cursor-pointer  transition ease-in-out duration-300 "
                 >
                   <img
@@ -185,7 +117,7 @@ export default function LoginMethod() {
                 </button>
 
                 <button
-                  onClick={handleLoginGit}
+                  // onClick={handleLoginGit}
                   className="flex items-center justify-items-center justify-center gap-[10px] w-full h-[45px] rounded-[10px] px-[20px] py-[10px] border-[1px] border-zinc-800 text-black cursor-pointer  transition ease-in-out duration-300 "
                 >
                   <img
