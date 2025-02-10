3f9ad2c2253928bf9e19ebe15d0588b4104e7abd
NXTA <nguyenxuantuongan6@gmail.com>
2025-02-10 10:18:31 +0700

    Update config

--- server/config/mongo.config.js
+++ server/config/mongo.config.js
@@ -1,19 +1,3 @@
-// require("dotenv").config();
-// const mongoose = require("mongoose");
-
-// mongoose.connect(process.env.MONGO_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-
-// const db = mongoose.connection;
-// db.on("error", console.error.bind(console, "MongoDB connection error:"));
-// db.once("open", () => {
-//   console.log("Connected to MongoDB Atlas!");
-// });
-
-// module.exports = { db };
-
 "use strict";
 require("dotenv").config();
 const mongoose = require("mongoose");
