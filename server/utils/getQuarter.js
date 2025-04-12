f79d623996e866afb158bd6ed4ad60dcd51a3746
NXTA <nguyenxuantuongan6@gmail.com>
2025-04-12 13:29:18 +0700

    fixed minor error

--- server/utils/getQuarter.js
+++ server/utils/getQuarter.js
@@ -1,7 +1,9 @@
-export function getQuarter(date) {
+function getQuarter(date) {
   const month = date.getMonth() + 1;
   if (month >= 1 && month <= 3) return "Q1";
   if (month >= 4 && month <= 6) return "Q2";
   if (month >= 7 && month <= 9) return "Q3";
   return "Q4";
 }
+
+module.exports = { getQuarter };
