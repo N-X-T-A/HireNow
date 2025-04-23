2a09a711d9851e4b7ec68124f3cf90e6ba276ef1
NXTA <nguyenxuantuongan6@gmail.com>
2025-04-23 09:47:54 +0700

    update title

--- recruiter/src/pages/Tables/JobListingTables.tsx
+++ recruiter/src/pages/Tables/JobListingTables.tsx
@@ -10,7 +10,7 @@ export default function JoblistingTables() {
         title="React.js Basic Tables Dashboard | TailAdmin - Next.js Admin Dashboard Template"
         description="This is React.js Basic Tables Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
       />
-      <PageBreadcrumb pageTitle="Applicants" />
+      <PageBreadcrumb pageTitle="Job Listing" />
       <div className="space-y-6">
         <ComponentCard title="">
           <JobListing />
