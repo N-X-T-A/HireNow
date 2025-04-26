3ad8297b0e234e0a4f71c1cb70d54c82ab36b363
NXTA <nguyenxuantuongan6@gmail.com>
2025-04-26 10:51:19 +0700

    change url config for deploy

--- recruiter/src/api/config.ts
+++ recruiter/src/api/config.ts
@@ -1,4 +1,4 @@
-const API_URL = "https://hirenow-da0t.onrender.com";
+const API_URL = "https://hirenow-server.onrender.com";
 const SERVICE_URL = `${API_URL}/api/v1`;
 
 export { API_URL, SERVICE_URL };
